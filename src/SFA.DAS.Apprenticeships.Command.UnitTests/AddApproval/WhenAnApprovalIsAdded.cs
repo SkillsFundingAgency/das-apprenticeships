70c9df5a311edf247be0bed0ebf73fe0218b2b6d
Michael Young <mick.young1981@googlemail.com>
2022-07-26 13:30:58 +0100

    Add command tests.

---
+++ src/SFA.DAS.Apprenticeships.Command.UnitTests/AddApproval/WhenAnApprovalIsAdded.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using AutoFixture;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Apprenticeships.Command.AddApproval;
+using SFA.DAS.Apprenticeships.Command.UnitTests.AutoFixture.Customizations;
+using SFA.DAS.Apprenticeships.Domain.Apprenticeship;
+using SFA.DAS.Apprenticeships.Domain.Factories;
+using SFA.DAS.Apprenticeships.Domain.Repositories;
+
+namespace SFA.DAS.Apprenticeships.Command.UnitTests.AddApproval
+{
+    [TestFixture]
+    public class WhenAnApprovalIsAdded
+    {
+        private AddApprovalCommandHandler _commandHandler;
+        private Mock<IApprenticeshipFactory> _apprenticeshipFactory;
+        private Mock<IApprenticeshipRepository> _apprenticeshipRepository;
+        private Fixture _fixture;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _apprenticeshipFactory = new Mock<IApprenticeshipFactory>();
+            _apprenticeshipRepository = new Mock<IApprenticeshipRepository>();
+            _commandHandler = new AddApprovalCommandHandler(_apprenticeshipFactory.Object, _apprenticeshipRepository.Object);
+
+            _fixture = new Fixture();
+            _fixture.Customize(new ApprenticeshipCustomization());
+        }
+
+        [Test]
+        public async Task ThenTheApprovalIsCreated()
+        {
+            var command = _fixture.Create<AddApprovalCommand>();
+            var apprenticeship = _fixture.Create<Apprenticeship>();
+
+            _apprenticeshipFactory.Setup(x => x.CreateNew(command.Uln, command.TrainingCode)).Returns(apprenticeship);
+
+            await _commandHandler.Handle(command);
+
+            _apprenticeshipRepository.Verify(x => x.Add(It.Is<Apprenticeship>(y => y.GetModel().Approvals.Count == 1)));
+        }
+    }
+}
---
+++ src/SFA.DAS.Apprenticeships.Command.UnitTests/AutoFixture/Customizations/ApprenticeshipCustomization.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AutoFixture;
+using SFA.DAS.Apprenticeships.Domain.Factories;
+
+namespace SFA.DAS.Apprenticeships.Command.UnitTests.AutoFixture.Customizations
+{
+    public class ApprenticeshipCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Register(() => new ApprenticeshipFactory().CreateNew(fixture.Create<long>(), fixture.Create<string>()));
+        }
+    }
+}
