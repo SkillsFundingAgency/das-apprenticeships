34f6876187a9dc7f4daabef58e33826a94319e68
ruslan <ruslanss@gmail.com>
2022-09-01 15:27:25 +0100

    Fix acceptance test: do not require settings file

--- src/AcceptanceTests/Helpers/WaitHelper.cs
+++ src/AcceptanceTests/Helpers/WaitHelper.cs
@@ -1,34 +1,10 @@
-﻿using Microsoft.Extensions.Configuration;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace SFA.DAS.Apprenticeships.AcceptanceTests.Helpers;
 
-public static class WaitConfigurationHelper
-{
-    public static WaitConfiguration WaitConfiguration
-    {
-        get
-        {
-            if (_waitConfiguration == null)
-            {
-                _waitConfiguration = new WaitConfiguration();
-                new ConfigurationBuilder()
-                    .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-                    .Build()
-                    .GetSection("WaitConfiguration")
-                    .Bind(_waitConfiguration);
-            }
-
-            return _waitConfiguration;
-        }
-    }
-
-    private static WaitConfiguration? _waitConfiguration;
-}
-
 public class WaitHelper
 {
-    private static WaitConfiguration Config => WaitConfigurationHelper.WaitConfiguration;
+    private static WaitConfiguration Config => new();
 
     public static async Task WaitForIt(Func<bool> lookForIt, string failText)
     {
