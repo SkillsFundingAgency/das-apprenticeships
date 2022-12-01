ecd4338d39da3e9ea9d4e89a68318d9fdcd86d77
Tom Gough <tom.gough@talentconsulting.uk>
2022-12-01 10:12:48 +0000

    Added unit test to cover non pilot scenario for age at start of apprenticeship

--- src/Domain.Tests/Apprenticeship/WhenCalculatingAgeAtStartOfApprenticeship.cs
+++ src/Domain.Tests/Apprenticeship/WhenCalculatingAgeAtStartOfApprenticeship.cs
@@ -25,8 +25,8 @@ namespace SFA.DAS.Apprenticeships.Domain.UnitTests.Apprenticeship
         [Test]
         public void ThenEarliestApprovalStartDateIsUsed()
         {
-            _apprenticeship.AddApproval(_fixture.Create<long>(), _fixture.Create<long>(), _fixture.Create<long>(), _fixture.Create<string>(), new DateTime(2022, 09, 01), _fixture.Create<DateTime>(), _fixture.Create<decimal>(), _fixture.Create<long>(), _fixture.Create<FundingType>(), _fixture.Create<int>(), _fixture.Create<DateTime?>(), _fixture.Create<bool?>());
-            _apprenticeship.AddApproval(_fixture.Create<long>(), _fixture.Create<long>(), _fixture.Create<long>(), _fixture.Create<string>(), new DateTime(2020, 11, 01), _fixture.Create<DateTime>(), _fixture.Create<decimal>(), _fixture.Create<long>(), _fixture.Create<FundingType>(), _fixture.Create<int>(), _fixture.Create<DateTime?>(), _fixture.Create<bool?>());
+            _apprenticeship.AddApproval(_fixture.Create<long>(), _fixture.Create<long>(), _fixture.Create<long>(), _fixture.Create<string>(), new DateTime(2022, 09, 01), _fixture.Create<DateTime>(), _fixture.Create<decimal>(), _fixture.Create<long>(), _fixture.Create<FundingType>(), _fixture.Create<int>(), _fixture.Create<DateTime?>(), false);
+            _apprenticeship.AddApproval(_fixture.Create<long>(), _fixture.Create<long>(), _fixture.Create<long>(), _fixture.Create<string>(), new DateTime(2020, 11, 01), _fixture.Create<DateTime>(), _fixture.Create<decimal>(), _fixture.Create<long>(), _fixture.Create<FundingType>(), _fixture.Create<int>(), _fixture.Create<DateTime?>(), false);
 
             _apprenticeship.AgeAtStartOfApprenticeship.Should().Be(20);
         }
@@ -34,7 +34,7 @@ namespace SFA.DAS.Apprenticeships.Domain.UnitTests.Apprenticeship
         [Test]
         public void WhenTheStartDateIsLaterInTheYearThenTheDateOfBirthThenAgeIsCorrect()
         {
-            _apprenticeship.AddApproval(_fixture.Create<long>(), _fixture.Create<long>(), _fixture.Create<long>(), _fixture.Create<string>(), new DateTime(2020, 11, 01), _fixture.Create<DateTime>(), _fixture.Create<decimal>(), _fixture.Create<long>(), _fixture.Create<FundingType>(), _fixture.Create<int>(), _fixture.Create<DateTime?>(), _fixture.Create<bool?>());
+            _apprenticeship.AddApproval(_fixture.Create<long>(), _fixture.Create<long>(), _fixture.Create<long>(), _fixture.Create<string>(), new DateTime(2020, 11, 01), _fixture.Create<DateTime>(), _fixture.Create<decimal>(), _fixture.Create<long>(), _fixture.Create<FundingType>(), _fixture.Create<int>(), _fixture.Create<DateTime?>(), false);
 
             _apprenticeship.AgeAtStartOfApprenticeship.Should().Be(20);
         }
@@ -42,9 +42,17 @@ namespace SFA.DAS.Apprenticeships.Domain.UnitTests.Apprenticeship
         [Test]
         public void WhenTheStartDateIsEarlierInTheYearThenTheDateOfBirthThenAgeIsCorrect()
         {
-            _apprenticeship.AddApproval(_fixture.Create<long>(), _fixture.Create<long>(), _fixture.Create<long>(), _fixture.Create<string>(), new DateTime(2020, 09, 01), _fixture.Create<DateTime>(), _fixture.Create<decimal>(), _fixture.Create<long>(), _fixture.Create<FundingType>(), _fixture.Create<int>(), _fixture.Create<DateTime?>(), _fixture.Create<bool?>());
+            _apprenticeship.AddApproval(_fixture.Create<long>(), _fixture.Create<long>(), _fixture.Create<long>(), _fixture.Create<string>(), new DateTime(2020, 09, 01), _fixture.Create<DateTime>(), _fixture.Create<decimal>(), _fixture.Create<long>(), _fixture.Create<FundingType>(), _fixture.Create<int>(), _fixture.Create<DateTime?>(), false);
 
             _apprenticeship.AgeAtStartOfApprenticeship.Should().Be(19);
+        }
+
+        [Test]
+        public void WhenCalculatingForANonPilotApprenticeThenAgeShouldBeNull()
+        {
+            _apprenticeship.AddApproval(_fixture.Create<long>(), _fixture.Create<long>(), _fixture.Create<long>(), _fixture.Create<string>(), null, _fixture.Create<DateTime>(), _fixture.Create<decimal>(), _fixture.Create<long>(), _fixture.Create<FundingType>(), _fixture.Create<int>(), new DateTime(2020, 11, 01), true);
+
+            _apprenticeship.AgeAtStartOfApprenticeship.Should().Be(null);
         }
     }
 }
