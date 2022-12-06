8405a1818d95240fc0565dd148374356b7a9b470
Michael Young <mick.young1981@googlemail.com>
2022-12-06 09:40:18 +0000

    Remove duplicate class

--- src/Types/QueueNames.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SFA.DAS.Apprenticeships.Types
-{
-    public static class QueueNames
-    {
-        public const string ApprovalCreated = "SFA.DAS.Apprenticeships.ApprovalCreated";
-    }
-}
