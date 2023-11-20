be960a25c755688c136556fb008f59f6dff97518
Tom Gough <tom.gough@talentconsulting.uk>
2023-11-20 15:35:21 +0000

    Unit tests for controller and query handlers

---
+++ src/SFA.DAS.Apprenticeships.InnerApi.UnitTests/Controllers/ApprenticeshipControllerTests/WhenGetApprenticeshipKey.cs
@@ -0,0 +1,42 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SFA.DAS.Apprenticeships.Command;
+using SFA.DAS.Apprenticeships.InnerApi.Controllers;
+using SFA.DAS.Apprenticeships.Queries;
+using SFA.DAS.Apprenticeships.Queries.GetApprenticeshipKey;
+
+namespace SFA.DAS.Apprenticeships.InnerApi.UnitTests.Controllers.ApprenticeshipControllerTests;
+
+public class WhenGetApprenticeshipKey
+{
+    private Fixture _fixture;
+    private Mock<IQueryDispatcher> _queryDispatcher;
+    private ApprenticeshipController _sut;
+
+    [SetUp]
+    public void Setup()
+    {
+        _fixture = new Fixture();
+        _queryDispatcher = new Mock<IQueryDispatcher>();
+        _sut = new ApprenticeshipController(_queryDispatcher.Object, Mock.Of<ICommandDispatcher>());
+    }
+
+    [Test]
+    public async Task ThenApprenticeshipPriceIsReturned()
+    {
+        var apprenticeshipHashedId = _fixture.Create<string>();
+        var expectedResult = _fixture.Create<Guid>();
+
+        _queryDispatcher
+            .Setup(x => x.Send<GetApprenticeshipKeyRequest, GetApprenticeshipKeyResponse>(It.Is<GetApprenticeshipKeyRequest>(r => r.ApprenticeshipHashedId == apprenticeshipHashedId)))
+            .ReturnsAsync(new GetApprenticeshipKeyResponse{ ApprenticeshipKey = expectedResult });
+
+        var result = await _sut.GetApprenticeshipKey(apprenticeshipHashedId);
+
+        result.Should().BeOfType<OkObjectResult>();
+        var okResult = (OkObjectResult)result;
+        okResult.Value.Should().Be(expectedResult);
+    }
+}
---
+++ src/SFA.DAS.Apprenticeships.Queries.UnitTests/WhenGetApprenticeshipPrice.cs
@@ -0,0 +1,42 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Moq;
+using SFA.DAS.Apprenticeships.DataTransferObjects;
+using SFA.DAS.Apprenticeships.Domain.Repositories;
+using SFA.DAS.Apprenticeships.Queries.GetApprenticeshipPrice;
+
+namespace SFA.DAS.Apprenticeships.Queries.UnitTests;
+
+public class WhenGetApprenticeshipPrice
+{
+    private Fixture _fixture;
+    private Mock<IApprenticeshipQueryRepository> _apprenticeshipQueryRepository;
+    private GetApprenticeshipPriceRequestQueryHandler _sut;
+
+    [SetUp]
+    public void Setup()
+    {
+
+        _fixture = new Fixture();
+        _apprenticeshipQueryRepository = new Mock<IApprenticeshipQueryRepository>();
+        _sut = new GetApprenticeshipPriceRequestQueryHandler(_apprenticeshipQueryRepository.Object);
+    }
+
+    [Test]
+    public async Task ThenApprenticeshipPriceIsReturned()
+    {
+        //Arrange
+        var query = _fixture.Create<GetApprenticeshipPriceRequest>();
+        var expectedResult = _fixture.Create<ApprenticeshipPrice>();
+
+        _apprenticeshipQueryRepository
+            .Setup(x => x.GetPrice(query.ApprenticeshipKey))
+            .ReturnsAsync(expectedResult);
+
+        //Act
+        var actualResult = await _sut.Handle(query);
+
+        //Assert
+        actualResult.Should().BeEquivalentTo(expectedResult);
+    }
+}
