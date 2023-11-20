61976d62503d8138b60055d9573fa545c2de6bb4
Tom Gough <tom.gough@talentconsulting.uk>
2023-11-20 11:52:23 +0000

    Removed unused usings and split out GetApprenticeshipKey classes

--- src/Queries/GetApprenticeshipKey/GetApprenticeshipKeyRequest.cs
+++ src/Queries/GetApprenticeshipKey/GetApprenticeshipKeyRequest.cs
@@ -1,30 +1,6 @@
-﻿using SFA.DAS.Apprenticeships.Domain.Repositories;
-using SFA.DAS.Apprenticeships.Queries.GetApprenticeshipPrice;
-
-namespace SFA.DAS.Apprenticeships.Queries.GetApprenticeshipKey;
+﻿namespace SFA.DAS.Apprenticeships.Queries.GetApprenticeshipKey;
 
 public class GetApprenticeshipKeyRequest : IQuery
 {
     public string ApprenticeshipHashedId { get; set; }
-}
-
-public class GetApprenticeshipKeyResponse
-{
-    public Guid? ApprenticeshipKey { get; set; }
-}
-
-public class GetApprenticeshipKeyRequestQueryHandler : IQueryHandler<GetApprenticeshipKeyRequest, GetApprenticeshipKeyResponse>
-{
-    private readonly IApprenticeshipQueryRepository _apprenticeshipQueryRepository;
-
-    public GetApprenticeshipKeyRequestQueryHandler(IApprenticeshipQueryRepository apprenticeshipQueryRepository)
-    {
-        _apprenticeshipQueryRepository = apprenticeshipQueryRepository;
-    }
-
-    public async Task<GetApprenticeshipKeyResponse> Handle(GetApprenticeshipKeyRequest query, CancellationToken cancellationToken = default)
-    {
-        var key = await _apprenticeshipQueryRepository.GetKey(query.ApprenticeshipHashedId);
-        return new GetApprenticeshipKeyResponse { ApprenticeshipKey = key };
-    }
 }
--- src/Queries/GetApprenticeshipPrice/GetApprenticeshipPriceRequest.cs
+++ src/Queries/GetApprenticeshipPrice/GetApprenticeshipPriceRequest.cs
@@ -1,11 +1,4 @@
-﻿using SFA.DAS.Apprenticeships.Queries.GetApprenticeships;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SFA.DAS.Apprenticeships.Queries.GetApprenticeshipPrice
+﻿namespace SFA.DAS.Apprenticeships.Queries.GetApprenticeshipPrice
 {
     public class GetApprenticeshipPriceRequest : IQuery
     {
