80326dae3200c144d02085a8a767b00dada92884
Tom Gough <tom.gough@talentconsulting.uk>
2023-11-30 16:23:39 +0000

    ApprenticeshipRepository fixed

--- src/Domain/Repositories/ApprenticeshipRepository.cs
+++ src/Domain/Repositories/ApprenticeshipRepository.cs
@@ -1,36 +1,54 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SFA.DAS.Apprenticeships.DataAccess;
+using SFA.DAS.Apprenticeships.Domain.Apprenticeship;
+using SFA.DAS.Apprenticeships.Domain.Factories;
 
 namespace SFA.DAS.Apprenticeships.Domain.Repositories
 {
     public class ApprenticeshipRepository : IApprenticeshipRepository
     {
         private readonly Lazy<ApprenticeshipsDataContext> _lazyContext;
+        private IDomainEventDispatcher _domainEventDispatcher;
+        private readonly IApprenticeshipFactory _apprenticeshipFactory;
         private ApprenticeshipsDataContext DbContext => _lazyContext.Value;
 
-        public ApprenticeshipRepository(Lazy<ApprenticeshipsDataContext> dbContext)
+        public ApprenticeshipRepository(Lazy<ApprenticeshipsDataContext> dbContext, IDomainEventDispatcher domainEventDispatcher, IApprenticeshipFactory apprenticeshipFactory)
         {
             _lazyContext = dbContext;
+            _domainEventDispatcher = domainEventDispatcher;
+            _apprenticeshipFactory = apprenticeshipFactory;
         }
 
-        public async Task Add(DataAccess.Entities.Apprenticeship.Apprenticeship apprenticeship)
+        public async Task Add(ApprenticeshipDomainModel apprenticeship)
         {
-            await DbContext.AddAsync(apprenticeship);
+            await DbContext.AddAsync(apprenticeship.GetEntity());
             await DbContext.SaveChangesAsync();
+            
+            foreach (dynamic domainEvent in apprenticeship.FlushEvents())
+            {
+                await _domainEventDispatcher.Send(domainEvent);
+            }
         }
 
-        public async Task<DataAccess.Entities.Apprenticeship.Apprenticeship> Get(Guid key)
+        public async Task<ApprenticeshipDomainModel> Get(Guid key)
         {
-            return await DbContext.Apprenticeships
+            var apprenticeship = await DbContext.Apprenticeships
                 .Include(x => x.Approvals)
                 .Include(x => x.PriceHistories)
                 .SingleAsync(x => x.Key == key);
+
+            return _apprenticeshipFactory.GetExisting(apprenticeship);
         }
 
-        public async Task Update(DataAccess.Entities.Apprenticeship.Apprenticeship apprenticeship)
+        public async Task Update(ApprenticeshipDomainModel apprenticeship)
         {
-            DbContext.Update(apprenticeship);
+            DbContext.Update(apprenticeship.GetEntity());
             await DbContext.SaveChangesAsync();
+            
+            foreach (dynamic domainEvent in apprenticeship.FlushEvents())
+            {
+                await _domainEventDispatcher.Send(domainEvent);
+            }
         }
     }
 }
--- src/Domain/Repositories/IApprenticeshipRepository.cs
+++ src/Domain/Repositories/IApprenticeshipRepository.cs
@@ -1,8 +1,10 @@
-﻿namespace SFA.DAS.Apprenticeships.Domain.Repositories;
+﻿using SFA.DAS.Apprenticeships.Domain.Apprenticeship;
+
+namespace SFA.DAS.Apprenticeships.Domain.Repositories;
 
 public interface IApprenticeshipRepository
 {
-    Task Add(DataAccess.Entities.Apprenticeship.Apprenticeship apprenticeship);
-    Task<DataAccess.Entities.Apprenticeship.Apprenticeship> Get(Guid key);
-    Task Update(DataAccess.Entities.Apprenticeship.Apprenticeship apprenticeship);
+    Task Add(ApprenticeshipDomainModel apprenticeship);
+    Task<ApprenticeshipDomainModel> Get(Guid key);
+    Task Update(ApprenticeshipDomainModel apprenticeship);
 }
