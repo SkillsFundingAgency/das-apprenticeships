e4dfe6a7f34e6ec6d70929f9d6d3dcb1f2d0f6b8
tom-gough <tom.gough.tgsd@outlook.com>
2024-02-21 11:36:37 +0000

    remove unused usings

--- src/Command/AddPriceHistory/CreateApprenticeshipPriceChangeCommandHandler.cs
+++ src/Command/AddPriceHistory/CreateApprenticeshipPriceChangeCommandHandler.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SFA.DAS.Apprenticeships.Domain.Apprenticeship;
-using SFA.DAS.Apprenticeships.Domain.Factories;
-using SFA.DAS.Apprenticeships.Domain.Repositories;
+﻿using SFA.DAS.Apprenticeships.Domain.Repositories;
 using SFA.DAS.Apprenticeships.Enums;
 
 namespace SFA.DAS.Apprenticeships.Command.AddPriceHistory
