a5132499477f2cec757d417764c40a344ae19dbd
dinathain <55487881+dinathain@users.noreply.github.com>
2024-03-20 16:54:39 +0000

    Remove unused repo method GetPriceHistory from corresponding interface

--- src/Domain/Repositories/IApprenticeshipQueryRepository.cs
+++ src/Domain/Repositories/IApprenticeshipQueryRepository.cs
@@ -6,8 +6,7 @@ namespace SFA.DAS.Apprenticeships.Domain.Repositories;
 public interface IApprenticeshipQueryRepository
 {
     Task<IEnumerable<DataTransferObjects.Apprenticeship>> GetAll(long ukprn, FundingPlatform? fundingPlatform);
-    Task<DataTransferObjects.ApprenticeshipPrice?> GetPrice(Guid apprenticeshipKey);
-    Task<IEnumerable<DataTransferObjects.ApprenticeshipPrice>> GetPriceHistory(Guid apprenticeshipKey);
+    Task<ApprenticeshipPrice?> GetPrice(Guid apprenticeshipKey);
     Task<PendingPriceChange?> GetPendingPriceChange(Guid apprenticeshipKey);
     Task<Guid?> GetKey(string apprenticeshipHashedId);
     Task<Guid?> GetKeyByApprenticeshipId(long apprenticeshipId);
