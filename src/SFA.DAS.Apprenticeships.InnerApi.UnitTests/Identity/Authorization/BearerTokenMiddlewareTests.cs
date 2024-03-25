ee3d50bf6de947dab0e2da2cf1657bf4b57e10df
dinathain <55487881+dinathain@users.noreply.github.com>
2024-03-25 22:34:28 +0000

    Add unit tests for bearer token middleware

---
+++ src/SFA.DAS.Apprenticeships.InnerApi.UnitTests/Identity/Authorization/BearerTokenMiddlewareTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using SFA.DAS.Apprenticeships.InnerApi.Identity.Authorization;
+using Microsoft.Extensions.Configuration;
+
+namespace SFA.DAS.Apprenticeships.InnerApi.UnitTests.Identity.Authorization
+{
+    internal class BearerTokenMiddlewareTests
+    {
+        private BearerTokenMiddleware _middleware;
+        private Mock<IConfiguration> _mockConfiguration;
+        private HttpContext _httpContext;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockConfiguration = new Mock<IConfiguration>();
+            _middleware = new BearerTokenMiddleware(Next, _mockConfiguration.Object);
+            _httpContext = new DefaultHttpContext();
+        }
+
+        private Task Next(HttpContext context)
+        {
+            return Task.CompletedTask;
+        }
+
+        [Test]
+        public async Task Invoke_WhenDisableAccountAuthorisationTrue_ShouldNotThrowException()
+        {
+            // Arrange
+            _mockConfiguration.Setup(x => x["DisableAccountAuthorisation"]).Returns("true");
+
+            // Act & Assert
+            Func<Task> action = async () => await _middleware.Invoke(_httpContext);
+            await action.Should().NotThrowAsync<UnauthorizedAccessException>();
+        }
+
+        [Test]
+        public async Task Invoke_WhenTokenMissing_ShouldThrowUnauthorizedAccessException()
+        {
+            // Arrange
+            _mockConfiguration.Setup(x => x["DisableAccountAuthorisation"]).Returns("false");
+
+            // Act & Assert
+            Func<Task> action = async () => await _middleware.Invoke(_httpContext);
+            await action.Should().ThrowAsync<UnauthorizedAccessException>();
+        }
+    }
+}
