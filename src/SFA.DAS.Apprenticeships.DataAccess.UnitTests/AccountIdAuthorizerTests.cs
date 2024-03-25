e267666aa85a460dfc830ccc3bef9c56ad72d239
dinathain <55487881+dinathain@users.noreply.github.com>
2024-03-25 22:01:01 +0000

    Added unit tests for key authorization logic

---
+++ src/SFA.DAS.Apprenticeships.DataAccess.UnitTests/AccountIdAuthorizerTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using AutoFixture;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Apprenticeships.DataAccess.Entities.Apprenticeship;
+using SFA.DAS.Apprenticeships.Enums;
+using SFA.DAS.Apprenticeships.Infrastructure;
+
+namespace SFA.DAS.Apprenticeships.DataAccess.UnitTests
+{
+    public class AccountIdAuthorizerTests
+    {
+        private Fixture _fixture;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fixture = new Fixture();
+        }
+
+        [Test]
+        public void ValidateAccountIds_ProviderAccountIdMismatch_ThrowsUnauthorizedAccessException()
+        {
+            // Arrange
+            var apprenticeship = _fixture.Create<Apprenticeship>();
+            apprenticeship.Ukprn = 12345;
+            var authorizer = SetUpAuthorizer(true, 54321, AccountIdClaimsType.Provider);
+
+            // Act & Assert
+            Assert.Throws<UnauthorizedAccessException>(() => authorizer.ValidateAccountIds(apprenticeship));
+        }
+
+        [Test]
+        public void ValidateAccountIds_EmployerAccountIdMismatch_ThrowsUnauthorizedAccessException()
+        {
+            // Arrange
+            var apprenticeship = new Apprenticeship { EmployerAccountId = 98765 };
+            var authorizer = SetUpAuthorizer(true, 56789, AccountIdClaimsType.Employer);
+
+            // Act & Assert
+            Assert.Throws<UnauthorizedAccessException>(() => authorizer.ValidateAccountIds(apprenticeship));
+        }
+
+        [Test]
+        public void ValidateAccountIds_NoClaimsValidationRequired_DoesNotThrowException()
+        {
+            // Arrange
+            var apprenticeship = new Apprenticeship();
+            var authorizer = SetUpAuthorizer(false);
+
+            // Act & Assert
+            Assert.DoesNotThrow(() => authorizer.ValidateAccountIds(apprenticeship));
+        }
+        
+        private AccountIdAuthorizer SetUpAuthorizer(bool isClaimsValidationRequired, long? accountId = null, AccountIdClaimsType? accountIdClaimsType = null)
+        {
+            var mockClaimsHandler = new Mock<IAccountIdClaimsHandler>();
+            mockClaimsHandler
+                .Setup(x => x.GetAccountIdClaims())
+                .Returns(new AccountIdClaims
+                {
+                    IsClaimsValidationRequired = isClaimsValidationRequired, AccountId = accountId, AccountIdClaimsType = accountIdClaimsType
+                });
+            return new AccountIdAuthorizer(mockClaimsHandler.Object);
+        }
+
+    }
+}
