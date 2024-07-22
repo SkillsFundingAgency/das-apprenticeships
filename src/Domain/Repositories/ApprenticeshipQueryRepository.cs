b7c3488243868931871fb781f20eefb3198ccbc7
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2024-07-22 10:00:09 +0100

    Fix get pending price change

--- src/Domain/Repositories/ApprenticeshipQueryRepository.cs
+++ src/Domain/Repositories/ApprenticeshipQueryRepository.cs
@@ -127,6 +127,7 @@ using SFA.DAS.Apprenticeships.Enums;
          {
              var apprenticeship = await DbContext.Apprenticeships
                  .Include(x => x.PriceHistories)
+                 .Include(x => x.Episodes).ThenInclude(x => x.Prices)
                  .Where(x => x.Key == apprenticeshipKey && x.PriceHistories.Any(y => y.PriceChangeRequestStatus == ChangeRequestStatus.Created))
                  .SingleOrDefaultAsync();
 
