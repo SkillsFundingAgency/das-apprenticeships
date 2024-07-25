bfcb9b60285b2bb8c7cb6a9c239bcdbb2b1e214d
dinathain <55487881+dinathain@users.noreply.github.com>
2024-07-25 15:07:51 +0100

    Added StartDate to ApprenticeshipStartDateChangedEvent

--- src/Types/ApprenticeshipStartDateChangedEvent.cs
+++ src/Types/ApprenticeshipStartDateChangedEvent.cs
@@ -4,6 +4,7 @@ public class ApprenticeshipStartDateChangedEvent : EpisodeUpdatedEvent
 {
     public Guid ApprenticeshipKey { get; set; }
     public long ApprenticeshipId { get; set; }
+    public DateTime StartDate { get; set; }
     public DateTime ApprovedDate { get; set; }
     public string ProviderApprovedBy { get; set; }
     public string EmployerApprovedBy { get; set; }
