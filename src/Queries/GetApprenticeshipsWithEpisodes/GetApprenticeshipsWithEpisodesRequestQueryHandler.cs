3d2f89ae66496c58085c33234f84a6dd8b556a5e
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2024-09-16 11:37:44 +0100

    Logging added

--- src/Queries/GetApprenticeshipsWithEpisodes/GetApprenticeshipsWithEpisodesRequestQueryHandler.cs
+++ src/Queries/GetApprenticeshipsWithEpisodes/GetApprenticeshipsWithEpisodesRequestQueryHandler.cs
@@ -1,22 +1,32 @@
-﻿using SFA.DAS.Apprenticeships.Domain.Repositories;
+﻿using Microsoft.Extensions.Logging;
+using SFA.DAS.Apprenticeships.Domain.Repositories;
 
 namespace SFA.DAS.Apprenticeships.Queries.GetApprenticeshipsWithEpisodes;
 
 public class GetApprenticeshipsWithEpisodesRequestQueryHandler : IQueryHandler<GetApprenticeshipsWithEpisodesRequest, GetApprenticeshipsWithEpisodesResponse?>
 {
     private readonly IApprenticeshipQueryRepository _apprenticeshipQueryRepository;
+    private readonly ILogger<GetApprenticeshipsWithEpisodesRequestQueryHandler> _logger;
 
-    public GetApprenticeshipsWithEpisodesRequestQueryHandler(IApprenticeshipQueryRepository apprenticeshipQueryRepository)
+    public GetApprenticeshipsWithEpisodesRequestQueryHandler(IApprenticeshipQueryRepository apprenticeshipQueryRepository, ILogger<GetApprenticeshipsWithEpisodesRequestQueryHandler> logger)
     {
         _apprenticeshipQueryRepository = apprenticeshipQueryRepository;
+        _logger = logger;
     }
 
     public async Task<GetApprenticeshipsWithEpisodesResponse?> Handle(GetApprenticeshipsWithEpisodesRequest query, CancellationToken cancellationToken = default)
     {
+        _logger.LogInformation("Handling GetApprenticeshipsWithEpisodesRequest for Ukprn: {ukprn}", query.Ukprn);
+
         var apprenticeships = await _apprenticeshipQueryRepository.GetApprenticeshipsWithEpisodes(query.Ukprn);
 
-        if (apprenticeships == null) return null;
+        if (apprenticeships == null)
+        {
+            _logger.LogInformation("No apprenticeships found for {ukprn}", query.Ukprn);
+            return null;
+        }
 
+        _logger.LogInformation("{numberFound} apprenticeships found for {ukprn}", apprenticeships.Count, query.Ukprn);
         return new GetApprenticeshipsWithEpisodesResponse(query.Ukprn, apprenticeships);
     }
 }
