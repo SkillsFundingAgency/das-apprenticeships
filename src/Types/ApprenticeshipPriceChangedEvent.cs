29cbdd325e8583d588fa3edb90aa7e7e2be57ac3
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2024-10-01 11:46:02 +0100

    Update ApprenticeshipPriceChangedEvent.cs

--- src/Types/ApprenticeshipPriceChangedEvent.cs
+++ src/Types/ApprenticeshipPriceChangedEvent.cs
@@ -2,7 +2,6 @@
 
 namespace SFA.DAS.Apprenticeships.Types;
 
-#pragma warning disable CS8618
 public class ApprenticeshipPriceChangedEvent : ApprenticeshipEvent
 {
     public long ApprenticeshipId { get; set; }
@@ -10,4 +9,3 @@ public class ApprenticeshipPriceChangedEvent : ApprenticeshipEvent
     public DateTime ApprovedDate { get; set; }
     public ApprovedBy ApprovedBy { get; set; }
 }
-#pragma warning restore CS8618
