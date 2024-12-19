4c8b42c50668f9db43991480fdd65c835c01f18c
Michael Young <mick.young1981@googlemail.com>
2024-12-19 10:59:33 +0000

    Add missing tests

--- src/SFA.DAS.Apprenticeships.Functions.UnitTests/WhenHandleApprovalCreatedTests.cs
+++ src/SFA.DAS.Apprenticeships.Functions.UnitTests/WhenHandleApprovalCreatedTests.cs
@@ -8,16 +8,24 @@ using SFA.DAS.Apprenticeships.Command;
 using SFA.DAS.Apprenticeships.Command.AddApprenticeship;
 using SFA.DAS.Apprenticeships.Enums;
 using SFA.DAS.CommitmentsV2.Messages.Events;
+using SFA.DAS.CommitmentsV2.Types;
 
 namespace SFA.DAS.Apprenticeships.Functions.UnitTests
 {
     public class WhenHandleApprovalCreatedTests
     {
+        private Fixture _fixture;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fixture = new Fixture();
+        }
+
         [Test]
         public async Task ThenApprovalIsAdded()
         {
-            var fixture = new Fixture();
-            var @event = fixture.Create<ApprenticeshipCreatedEvent>();
+            var @event = _fixture.Create<ApprenticeshipCreatedEvent>();
             var commandDispatcher = new Mock<ICommandDispatcher>();
             var handler = new HandleApprovalCreated(commandDispatcher.Object);
             await handler.HandleCommand(@event, new Mock<ILogger>().Object);
@@ -38,9 +46,64 @@ namespace SFA.DAS.Apprenticeships.Functions.UnitTests
                         c.UKPRN == @event.ProviderId &&
                         c.Uln == @event.Uln &&
                         c.DateOfBirth == @event.DateOfBirth &&
-                        c.FundingPlatform == (@event.IsOnFlexiPaymentPilot.HasValue ? (@event.IsOnFlexiPaymentPilot.Value ? FundingPlatform.DAS : FundingPlatform.SLD) : null)
+                        c.FirstName == @event.FirstName &&
+                        c.LastName == @event.LastName &&
+                        c.ApprenticeshipHashedId == @event.ApprenticeshipHashedId &&
+                        c.FundingPlatform == (@event.IsOnFlexiPaymentPilot.HasValue ? (@event.IsOnFlexiPaymentPilot.Value ? FundingPlatform.DAS : FundingPlatform.SLD) : null) &&
+                        c.AccountLegalEntityId == @event.AccountLegalEntityId &&
+                        c.TrainingCourseVersion == @event.TrainingCourseVersion
                     ),
                     It.IsAny<CancellationToken>()));
+        }
+
+        [Test]
+        public async Task WhenEmployerIsLevyThenFundingTypeIsLevy()
+        {
+            var @event = _fixture.Build<ApprenticeshipCreatedEvent>().With(x => x.TransferSenderId, (long?)null).With(x => x.ApprenticeshipEmployerTypeOnApproval, ApprenticeshipEmployerType.Levy).Create();
+            var commandDispatcher = new Mock<ICommandDispatcher>();
+            var handler = new HandleApprovalCreated(commandDispatcher.Object);
+            await handler.HandleCommand(@event, new Mock<ILogger>().Object);
+
+            commandDispatcher.Verify(x =>
+                x.Send(It.Is<AddApprenticeshipCommand>(c => c.FundingType == Enums.FundingType.Levy),
+                    It.IsAny<CancellationToken>()));
+        }
+
+        [Test]
+        public async Task WhenEmployerIsNonLevyThenFundingTypeIsNonLevy()
+        {
+            var @event = _fixture.Build<ApprenticeshipCreatedEvent>().With(x => x.TransferSenderId, (long?)null).With(x => x.ApprenticeshipEmployerTypeOnApproval, ApprenticeshipEmployerType.NonLevy).Create();
+            var commandDispatcher = new Mock<ICommandDispatcher>();
+            var handler = new HandleApprovalCreated(commandDispatcher.Object);
+            await handler.HandleCommand(@event, new Mock<ILogger>().Object);
+
+            commandDispatcher.Verify(x =>
+                x.Send(It.Is<AddApprenticeshipCommand>(c => c.FundingType == Enums.FundingType.NonLevy),
+                    It.IsAny<CancellationToken>()));
+        }
+
+        [Test]
+        public async Task WhenHasTransferSenderThenFundingTypeIsTransfer()
+        {
+            var @event = _fixture.Build<ApprenticeshipCreatedEvent>().With(x => x.TransferSenderId, 1234).Create();
+            var commandDispatcher = new Mock<ICommandDispatcher>();
+            var handler = new HandleApprovalCreated(commandDispatcher.Object);
+            await handler.HandleCommand(@event, new Mock<ILogger>().Object);
+
+            commandDispatcher.Verify(x =>
+                x.Send(It.Is<AddApprenticeshipCommand>(c => c.FundingType == Enums.FundingType.Transfer),
+                    It.IsAny<CancellationToken>()));
+        }
+
+        [Test]
+        public async Task WhenIsNotOnPilotThenApprenticeshipNotCreated()
+        {
+            var @event = _fixture.Build<ApprenticeshipCreatedEvent>().With(x => x.IsOnFlexiPaymentPilot, false).Create();
+            var commandDispatcher = new Mock<ICommandDispatcher>();
+            var handler = new HandleApprovalCreated(commandDispatcher.Object);
+            await handler.HandleCommand(@event, new Mock<ILogger>().Object);
+
+            commandDispatcher.Verify(x => x.Send(It.IsAny<AddApprenticeshipCommand>(), It.IsAny<CancellationToken>()), Times.Never);
         }
     }
 }
