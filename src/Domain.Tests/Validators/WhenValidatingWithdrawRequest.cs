231c368b14f84f712b8828a110922e80580c3bfa
Robert Rybnikar <robert.rybnikar@education.gov.uk>
2025-01-17 14:21:16 +0000

    FLP-709 Auto cancel pending requests following a withdrawal - unit tests to prove functionality in place

--- src/Domain.Tests/Validators/WhenValidatingWithdrawRequest.cs
+++ src/Domain.Tests/Validators/WhenValidatingWithdrawRequest.cs
@@ -97,6 +97,24 @@ public class WhenValidatingWithdrawRequest
         validationMessage.Should().Contain("Apprenticeship already withdrawn for ULN");
     }
 
+    [Test]
+    public void WhenApprenticeshipIsWithdrawnThenCancelAnyPendingRequests()
+    {
+        // Arrange
+        var validator = new WithdrawValidator(_systemClockService.Object);
+        var apprenticeship = ApprenticeshipDomainModelTestHelper.CreateBasicTestModel();
+        ApprenticeshipDomainModelTestHelper.AddEpisode(apprenticeship, ukprn: ValidUkprn);
+        ApprenticeshipDomainModelTestHelper.AddPendingStartDateChange(apprenticeship, Enums.ChangeInitiator.Employer, DateTime.UtcNow);
+        ApprenticeshipDomainModelTestHelper.AddPendingPriceChangeProviderInitiated(apprenticeship, DateTime.UtcNow);
+
+        // Act
+        apprenticeship.WithdrawApprenticeship("ProviderApprovedBy", DateTime.UtcNow, "Reason", DateTime.UtcNow);
+
+        // Assert
+        apprenticeship.PendingStartDateChange.Should().BeNull();
+        apprenticeship.PendingPriceChange.Should().BeNull();
+    }
+
     [TestCase("Invalid reason, possible values are", "InvalidValue", "")]
     [TestCase("Reason text is required for 'Other' reason", "Other", "")]
     [TestCase("Reason text must be less than 100 characters", "Other", StringLongerThan100)]
