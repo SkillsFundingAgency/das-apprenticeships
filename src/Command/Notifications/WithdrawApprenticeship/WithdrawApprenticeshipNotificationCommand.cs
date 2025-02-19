a4d0b6ab6704e63ef196e5e615fc633df76c2f4f
tom-gough <tom.gough.tgsd@outlook.com>
2025-02-19 08:24:53 +0000

    FLP-710 WIP

--- src/Command/Notifications/WithdrawApprenticeship/WithdrawApprenticeshipNotificationCommand.cs
+++ src/Command/Notifications/WithdrawApprenticeship/WithdrawApprenticeshipNotificationCommand.cs
@@ -10,7 +10,10 @@ namespace SFA.DAS.Apprenticeships.Command.Notifications.WithdrawApprenticeship
 {
     public class WithdrawApprenticeshipNotificationCommand : ICommand
     {
-
+        public long UKPRN { get; set; }
+        public long ApprovalsApprenticeshipId { get; set; }
+        public string ApprenticeFirstName { get; set; }
+        public string ApprenticeLastName { get; set; }
     }
 
     public class WithdrawApprenticeshipNotificationCommandHandler : ICommandHandler<WithdrawApprenticeshipNotificationCommand, Outcome>
@@ -18,6 +21,9 @@ namespace SFA.DAS.Apprenticeships.Command.Notifications.WithdrawApprenticeship
         public async Task<Outcome> Handle(WithdrawApprenticeshipNotificationCommand command,
             CancellationToken cancellationToken = default(CancellationToken))
         {
+            //todo
+            //call commitments - inner? outer? to get provider name, employer name, apprentice name we have but if it comes back on the same call is it better to use the version from commitments?
+            //[Route("/provider/{providerId}/apprentices/{apprenticeshipId}/details")] call the outer like this
             throw new NotImplementedException();
         }
     }
