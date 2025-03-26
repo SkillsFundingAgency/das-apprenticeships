c4febe4afdf9c0e6c22a3ec237e01fcc33f4e525
Jon Harris <jon.harris@fuitsu.com>
2025-03-26 08:36:32 +0000

    UserTypeRequirement.AuthorizeMode.Override setting on GetByDates

--- src/InnerApi/Controllers/ApprenticeshipController.cs
+++ src/InnerApi/Controllers/ApprenticeshipController.cs
@@ -68,7 +68,7 @@ public class ApprenticeshipController : ControllerBase
     /// <returns>GetApprenticeshipsByDatesResponse</returns>
     [HttpGet("{ukprn:long}/apprenticeships/by-dates")]
     [ProducesResponseType(typeof(GetApprenticeshipsByDatesResponse), 200)]
-    [AuthorizeUserType(UserType.ServiceAccount)]
+    [AuthorizeUserType(UserType.ServiceAccount, UserTypeRequirement.AuthorizeMode.Override)]
     public async Task<IActionResult> GetByDates(long ukprn, [FromQuery] string startDate, [FromQuery] string endDate, [FromQuery] int page = 1, [FromQuery] int? pageSize = null)
     {
         var isValidStartDate = DateTime.TryParse(startDate, out var startDateValue);
