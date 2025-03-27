2c97b4b5c4d96bace10a482911df5dcd56f586ee
Jon Harris <jon.harris@fuitsu.com>
2025-03-27 12:24:32 +0000

    Using allowanonymous for testing

--- src/InnerApi/Controllers/ApprenticeshipController.cs
+++ src/InnerApi/Controllers/ApprenticeshipController.cs
@@ -68,7 +68,8 @@ public class ApprenticeshipController : ControllerBase
     /// <returns>GetApprenticeshipsByDatesResponse</returns>
     [HttpGet("{ukprn:long}/apprenticeships/by-dates")]
     [ProducesResponseType(typeof(GetApprenticeshipsByDatesResponse), 200)]
-    [ActionAuthorizeUserType(UserType.ServiceAccount)]
+    //[ActionAuthorizeUserType(UserType.ServiceAccount)]
+    [AllowAnonymous]
     public async Task<IActionResult> GetByDates(long ukprn, [FromQuery] string startDate, [FromQuery] string endDate, [FromQuery] int page = 1, [FromQuery] int? pageSize = null)
     {
         var isValidStartDate = DateTime.TryParse(startDate, out var startDateValue);
