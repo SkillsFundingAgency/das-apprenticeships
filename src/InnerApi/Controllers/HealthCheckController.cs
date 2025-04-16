c71eba22ca8a7bb28f442df3d7347c99b2f9878d
ogrwill <will.ogrady@digital.education.gov.uk>
2025-04-16 15:02:35 +0100

    removing previous Health check endpoint

--- src/InnerApi/Controllers/HealthCheckController.cs
+++ src/InnerApi/Controllers/HealthCheckController.cs
@@ -9,14 +9,7 @@ namespace SFA.DAS.Apprenticeships.InnerApi.Controllers;
 [ApiController]
 [AllowAnonymous]
 public class HealthCheckController(IConfiguration config) : Controller
-{
-    [HttpGet("")]
-    [ProducesResponseType(typeof(string), 200)]
-    public IActionResult Index()
-    {
-        return new OkObjectResult("Healthy");
-    }
-
+{   
     [HttpGet("info")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public IActionResult GetInfo()
