b4a1cd19d64736a72aa54e93cdc131374b3d2329
Jon Harris <jon.harris@fuitsu.com>
2025-04-22 10:51:17 +0100

    Healthcheck fixes

--- src/InnerApi/Extensions/HealthCheckStartupExtensions.cs
+++ src/InnerApi/Extensions/HealthCheckStartupExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.Json;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using SFA.DAS.Apprenticeships.DataAccess;
+using Microsoft.Data.SqlClient;
+using SFA.DAS.Apprenticeships.DataAccess.Extensions;
 using SFA.DAS.Apprenticeships.Infrastructure.Configuration;
 
 namespace SFA.DAS.Apprenticeships.InnerApi.Extensions;
@@ -13,13 +14,20 @@ public static class HealthCheckStartupExtensions
     /// <summary>
     /// Add health-checks
     /// </summary>
-    /// <param name="services"></param>
+    /// <param name="services">IServiceCollection</param>
+    /// <param name="appSettings">Application Settings instance.</param>
     /// <returns></returns>
     public static IServiceCollection AddDasHealthChecks(this IServiceCollection services, ApplicationSettings appSettings)
     {
         services
             .AddHealthChecks()
-            .AddSqlServer(appSettings.DbConnectionString);
+            .AddSqlServer(appSettings.DbConnectionString, beforeOpenConnectionConfigurer: connection =>
+            {
+                {
+                    var conn = DatabaseExtensions.GetSqlConnection(appSettings.DbConnectionString);
+                    connection.AccessToken = ((SqlConnection)conn).AccessToken;
+                }
+            });
 
         return services;
     }
