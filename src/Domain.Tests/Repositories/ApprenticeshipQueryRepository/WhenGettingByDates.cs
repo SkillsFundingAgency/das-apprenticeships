3a15b585a5155b5d0c06f3a075bdcb42e49a06ba
Jon Harris <jon.harris@fuitsu.com>
2025-04-23 08:04:19 +0100

    Test amendments

--- src/Domain.Tests/Repositories/ApprenticeshipQueryRepository/WhenGettingByDates.cs
+++ src/Domain.Tests/Repositories/ApprenticeshipQueryRepository/WhenGettingByDates.cs
@@ -121,29 +121,6 @@ public class WhenGettingByDates
         result.Data.Select(x => x.Uln).Should().NotContain(apprenticeship4.Uln);
     }
 
-    [Test]
-    public async Task ThenCorrectApprenticeshipsForUkprnAreRetrievedForOutsideAcademicYearAndActive()
-    {
-        // Arrange
-        var ukprn = _fixture.Create<long>();
-        var academicYear = new DateRange(new DateTime(2025, 8, 1), new DateTime(2026, 7, 31));
-        SetUpApprenticeshipQueryRepository();
-
-        var apprenticeship1 = await _dbContext.AddApprenticeship(_fixture.Create<Guid>(), false, ukprn, startDate: academicYear.Start.AddDays(1), learnerStatus: LearnerStatus.Active);
-        var apprenticeship2 = await _dbContext.AddApprenticeship(_fixture.Create<Guid>(), false, ukprn, startDate: academicYear.Start.AddDays(2), learnerStatus: LearnerStatus.Active);
-        var apprenticeship3 = await _dbContext.AddApprenticeship(_fixture.Create<Guid>(), false, ukprn, startDate: academicYear.Start.AddDays(3), learnerStatus: LearnerStatus.Active);
-        var apprenticeship4 = await _dbContext.AddApprenticeship(_fixture.Create<Guid>(), false, ukprn, startDate: academicYear.Start.AddDays(4), learnerStatus: LearnerStatus.Active);
-
-        var result = await _sut.GetByDates(ukprn, academicYear, 100, 0, CancellationToken.None);
-
-        result.Should().NotBeNull();
-        result.Data.Count().Should().Be(0);
-        result.Data.Select(x => x.Uln).Should().NotContain(apprenticeship1.Uln);
-        result.Data.Select(x => x.Uln).Should().NotContain(apprenticeship2.Uln);
-        result.Data.Select(x => x.Uln).Should().NotContain(apprenticeship3.Uln);
-        result.Data.Select(x => x.Uln).Should().NotContain(apprenticeship4.Uln);
-    }
-
     private void SetUpApprenticeshipQueryRepository()
     {
         _dbContext = InMemoryDbContextCreator.SetUpInMemoryDbContext();
