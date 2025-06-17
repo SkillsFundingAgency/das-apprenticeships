e4e98d6f6421008f851a53e749caab463ede6d04
Chris Foster <chrisfoster186@googlemail.com>
2025-06-17 11:29:13 +0100

    Renaming phase 2

--- src/Database/Scripts/PostDeployment.sql
+++ src/Database/Scripts/PostDeployment.sql
@@ -1,17 +1,3 @@
 ﻿/*
 Post-deployment script
 */
-
-IF EXISTS (SELECT 1 FROM [Learning])
-BEGIN
-    PRINT 'ERROR: Learning table already populated; aborting.';
-    RETURN;
-END
-GO
-
-/* Learning (copy of Apprenticeship) */
-
-EXEC('
-    INSERT INTO [dbo].[Learning] ([Key], [ApprovalsApprenticeshipId], [Uln], [FirstName], [LastName], [DateOfBirth], [ApprenticeshipHashedId])
-    SELECT [Key], [ApprovalsApprenticeshipId], [Uln], [FirstName], [LastName], [DateOfBirth], [ApprenticeshipHashedId]
-    FROM [dbo].[Apprenticeship];');
