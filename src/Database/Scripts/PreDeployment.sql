242a64b822dd50912129f26c5b0a9a9379b5ba06
Chris Foster <chrisfoster186@googlemail.com>
2025-06-20 05:40:50 +0100

    Temporarily removes script

--- src/Database/Scripts/PreDeployment.sql
+++ src/Database/Scripts/PreDeployment.sql
@@ -1,17 +1,17 @@
-﻿/* Pre-deployment script */
+﻿--/* Pre-deployment script */
 
-EXEC('
-    INSERT INTO [dbo].[Learning] ([Key], [ApprovalsApprenticeshipId], [Uln], [FirstName], [LastName], [DateOfBirth], [ApprenticeshipHashedId])
-    SELECT A.[Key], A.[ApprovalsApprenticeshipId], A.[Uln], A.[FirstName], A.[LastName], A.[DateOfBirth], A.[ApprenticeshipHashedId]
-    FROM [dbo].[Apprenticeship] A
-    WHERE NOT EXISTS (
-        SELECT 1
-        FROM [dbo].[Learning] L
-        WHERE L.[Key] = A.[Key]
-    );');
+--EXEC('
+--    INSERT INTO [dbo].[Learning] ([Key], [ApprovalsApprenticeshipId], [Uln], [FirstName], [LastName], [DateOfBirth], [ApprenticeshipHashedId])
+--    SELECT A.[Key], A.[ApprovalsApprenticeshipId], A.[Uln], A.[FirstName], A.[LastName], A.[DateOfBirth], A.[ApprenticeshipHashedId]
+--    FROM [dbo].[Apprenticeship] A
+--    WHERE NOT EXISTS (
+--        SELECT 1
+--        FROM [dbo].[Learning] L
+--        WHERE L.[Key] = A.[Key]
+--    );');
 
-EXEC('UPDATE [Episode] set [LearningKey] = [ApprenticeshipKey];');
-EXEC('UPDATE [FreezeRequest] set [LearningKey] = [ApprenticeshipKey];');
-EXEC('UPDATE [PriceHistory] set [LearningKey] = [ApprenticeshipKey];');
-EXEC('UPDATE [StartDateChange] set [LearningKey] = [ApprenticeshipKey];');
-EXEC('UPDATE [WithdrawalRequest] set [LearningKey] = [ApprenticeshipKey];');
+--EXEC('UPDATE [Episode] set [LearningKey] = [ApprenticeshipKey];');
+--EXEC('UPDATE [FreezeRequest] set [LearningKey] = [ApprenticeshipKey];');
+--EXEC('UPDATE [PriceHistory] set [LearningKey] = [ApprenticeshipKey];');
+--EXEC('UPDATE [StartDateChange] set [LearningKey] = [ApprenticeshipKey];');
+--EXEC('UPDATE [WithdrawalRequest] set [LearningKey] = [ApprenticeshipKey];');
