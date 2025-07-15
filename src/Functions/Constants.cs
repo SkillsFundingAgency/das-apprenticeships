bb8fb260f7fd8c934a66cf02ad9614db1f9f8a52
Chris Foster <chrisfoster186@googlemail.com>
2025-07-15 12:03:49 +0100

    Reinstates temporary duplicate function

--- src/Functions/Constants.cs
+++ src/Functions/Constants.cs
@@ -2,6 +2,7 @@
 {
     public static class Constants
     {
+        public const string OldEndpointName = "SFA.DAS.Apprenticeships";
         public const string EndpointName = "SFA.DAS.Learning";
     }
 }
